659748a0a249dc245ebae339c20dcbb1874f4563
Karolina Lencina <karolencina@icloud.com>
2023-07-20 17:07:26 +0100

    Initial Setup

---
+++ Feelgraph/EntryCell.swift
@@ -0,0 +1,8 @@
+//
+//  EntryCell.swift
+//  Feelgraph
+//
+//  Created by Karolina Lencina on 19/07/2023.
+//
+
+import Foundation
---
+++ Feelgraph/EntryDetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  EntryDetailVC.swift
+//  Feelgraph
+//
+//  Created by Karolina Lencina on 19/07/2023.
+//
+
+import UIKit
+
+class EntryDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
